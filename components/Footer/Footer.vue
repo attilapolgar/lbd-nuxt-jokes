3d2df9e14b6a1ee3221a6dea3684d4f3c89ce05e
Attila Polgar <raglopa@gmail.com>
2018-04-03 11:16:41 +0200

    feat(layout): add header and footer

---
+++ components/Footer/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <b-container>
+    <footer class="bd-footer text-muted">
+      <div class="container">
+        <b-nav>
+          <b-nav-item class="">github</b-nav-item>
+          <b-nav-item class="">linkedin</b-nav-item>
+        </b-nav>
+      </div>
+    </footer>
+  </b-container>
+</template>
---
+++ components/Header/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <b-navbar toggleable="md" type="dark" variant="info">
+
+      <b-navbar-brand href="#">project title</b-navbar-brand>
+
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item>linkedin</b-nav-item>
+        <b-nav-item>github</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
